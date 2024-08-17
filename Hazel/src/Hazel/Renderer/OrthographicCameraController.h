66de1c90d8259e937fea0e793c2555217254958a
SevenEggs <997481408@qq.com>
2024-08-17 17:18:21 +0800

    Upgrade to VS 2022 + Organization

--- Hazel/src/Hazel/Renderer/OrthographicCameraController.h
+++ Hazel/src/Hazel/Renderer/OrthographicCameraController.h
@@ -21,6 +21,9 @@ namespace Hazel {
 	private:
 		bool OnMouseScrolled(MouseScrolledEvent& e);
 		bool OnWindowResized(WindowResizeEvent& e);
+
+		float GetZoomLevel() const { return m_ZoomLevel; }
+		void SetZoomLevel(float level) { m_ZoomLevel = level; }
 	private:
         float m_AspectRatio;
         float m_ZoomLevel = 1.0f;
